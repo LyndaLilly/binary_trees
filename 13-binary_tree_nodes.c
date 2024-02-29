dab0aa351d2162bd9e044982195e09be22ef7148
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-02-29 09:55:08 -0800

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,12 +7,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t cnt = 0;
+	size_t cnt = 0;
 
-if (tree == NULL)
-return (0);
-if (tree->right != NULL || tree->left != NULL)
-cnt += binary_tree_nodes(tree->left) + 1
-+ binary_tree_nodes(tree->right);
-return (cnt);
+	if (tree == NULL)
+		return (0);
+	if (tree->right != NULL || tree->left != NULL)
+		cnt += binary_tree_nodes(tree->left) + 1
+			+ binary_tree_nodes(tree->right);
+	return (cnt);
 }
