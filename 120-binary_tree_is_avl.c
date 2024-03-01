d80bb005ef07effcfdc9ec1d8f2e4874301a29ec
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-03-01 12:41:12 -0800

    task 30

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_avl - this is the binary tree
+ * binary_tree_is_avl - this is the binary tre
  * @tree: this i she tree
- * Return: 1 or  0 
+ * Return: 1 or  0
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
-return (1);
+	if (tree == NULL)
+		return (0);
+	return (1);
 }
